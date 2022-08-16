6f4613bf9b2edc419c0fb492fdea100b97fd0b80
ezzywizzy <nassaryezekiel@gmail.com>
2022-08-16 21:36:55 +0300

    creating indent purchase form

--- spareparts_management.php
+++ spareparts_management.php
@@ -9,63 +9,81 @@ include "templates/top.php";
             <div class="card-body ">
               <h5 class="card-title">Spareparts Management</h5>
 
-              <!-- Bordered Tabs Justified -->
-              <ul class="nav nav-tabs nav-tabs-bordered d-flex" id="borderedTabJustified" role="tablist">
-                <li class="nav-item flex-fill" role="presentation">
-                  <button class="nav-link w-100 active" id="home-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-home" type="button" role="tab" aria-controls="home" aria-selected="true">NEW SPAREPART</button>
-                </li>
-                <li class="nav-item flex-fill" role="presentation">
-                  <button class="nav-link w-100" id="profile-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-profile" type="button" role="tab" aria-controls="profile" aria-selected="false">STOCK</button>
-                </li>
-                <li class="nav-item flex-fill" role="presentation">
-                  <button class="nav-link w-100" id="contact-tab" data-bs-toggle="tab" data-bs-target="#bordered-justified-contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
-                </li>
-              </ul>
-              <div class="tab-content pt-2" id="borderedTabJustifiedContent">
-                <div class="tab-pane fade show active" id="bordered-justified-home" role="tabpanel" aria-labelledby="home-tab">
-                
+             
+            
 <div class="card pt-2">
       <div class="card-body">
         <div class="row">
-                  <div class="col-lg-4 p-2 border">
-      <h1 class="card-title shadow-sm p-3 text-light  bg-success rounded">Spare Code Registration</h1>
-         <!-- Floating Labels Form -->
-              <form class="row g-3">
-                <div class="col-md-12">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="partname" placeholder="Partname">
-                    <label for="partname">Part Name</label>
+                  <div class="col-lg-12 p-2 mt-3  border">
+                    <nav class="navbar navbar-light bg-light p-2 shadow-sm">
+  <a class="navbar-brand ml-2">General</a>
+ <p>Setting</p>
+  
+</nav>
+ 
+         <!-- spare register Form -->
+         <form class="row g-3 mt-3 " id="spareparts-form">
+          <div class="row">
+             <div class="col-lg-6">
+         <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Sparepart Name:</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" id="inputText">
                   </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="partnumber" placeholder="Partnumber">
-                    <label for="floatingEmail">Code No.</label>
+                 </div>
+                 <div class="col-lg-2">
+                 </div>
+                  <div class="col-lg-4">
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Serial No:</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" id="inputText">
                   </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="manufacturer" placeholder="Manufacturer">
-                    <label for="floatingEmail">Manufacturer</label>
-                  </div>
+                 </div>
+                 <div class="col-lg-6">
+         <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Description:</label>
+                  <div class="col-sm-8">
+                   <textarea class="form-control" placeholder="enter details about a sparepart" id="description" style="height: 100px;"></textarea>
+                    </div>
                 </div>
-                <div class="col-12">
-                  <div class="form-floating">
-                    <textarea class="form-control" placeholder="enter details about a sparepart" id="description" style="height: 100px;"></textarea>
-                    <label for="description">Description</label>
-                  </div>
-                </div>
-                <div class="col-md-12">
-                   <div class="form-floating mb-3">
-                  <select class="form-select" id="Category" aria-label="State">
+                 </div>
+                 <div class="col-lg-2">
+                 </div>
+                  <div class="col-lg-4">
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Category:</label>
+                  <div class="col-sm-8">
+                   <select class="form-select" id="Category" aria-label="State">
                     <option selected value="mech">Electrical</option>
                       <option value="electrical">Mechanical</option>
                   </select>
-                   <label for="Category">Category</label>
+                  </div>
                 </div>
+                 </div>
+                 <hr>
+                <div class="col-lg-4">
+         <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Quantity</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" id="qty" placeholder="Enter quantity">
                 </div>
-                <div class="col-md-12">
-                  <div class="form-floating mb-3">
+                 </div>
+                  </div>
+                 <div class="col-lg-4">
+                 <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Cost</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" id="qty" placeholder="Enter Cost">
+                </div>
+                 </div>
+                 </div>
+                  <div class="col-lg-4">
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Type:</label>
+                  <div class="col-sm-8">
                     <select class="form-select" id="type" aria-label="State">
                       <option selected value="motor">Motor</option>
                       <option value="drive">Drive</option>
@@ -92,44 +110,44 @@ include "templates/top.php";
                        <option value="cylinders">Cams</option>
                    
                       </select>
-                    <label for="Category">Type</label>
                   </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="qty" placeholder="Enter quantity">
-                    <label for="qty">Quantity</label>
+                 </div>
+ <div class="col-lg-4">
+         <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Order Level</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" id="qty" placeholder="Enter order level">
+                </div>
+                 </div>
                   </div>
+                 <div class="col-lg-4">
+                 <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Manufacturer</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" id="qty" placeholder="Enter manufacturer">
                 </div>
-              <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="amount" placeholder="Enter amount">
-                    <label for="amount">Amount (Tzs)</label>
-                  </div>
+                 </div>
+                 </div>
+                  <div class="col-lg-4">
+                 <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-4 col-form-label">Date</label>
+                  <div class="col-sm-8">
+                    <input type="date" class="form-control" id="date" placeholder="Enter date">
                 </div>
-                <div class="col-md-6">
-                  <div class="form-floating">
-                    <input type="text" class="form-control" id="amount" placeholder="Enter order level">
-                    <label for="amount">Order Level</label>
-                  </div>
-                </div>
-                <div class="col-md-12">
-                            <div class="form-check">
-                      <input class="form-check-input" type="checkbox" id="openingstock" checked>
-                      <label class="form-check-label" for="openingstock">
-                       Opening Stock
-                      </label>
-                    </div>
-                </div>
-                <div class="text-center text-success">
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                  <button type="reset" class="btn btn-secondary">Reset</button>
-                </div>
-              </form><!-- End floating Labels Form -->
-            
-      
-    </div>
-    <div class="col-lg-8">
+                 </div>
+                 </div>
+</form>
+  </div>
+   </div>
+<hr>
+
+      <div class="col-lg-12    border">
+ <nav class="navbar navbar-light bg-light p-2 shadow-sm">
+  <a class="navbar-brand ml-2">General</a>
+ <p>Setting</p>
+  
+</nav>
     <h1 class="card-title text-success  text-center">Spareparts Registered</h1>
  <table class="table datatable">
             <thead>
@@ -161,21 +179,11 @@ include "templates/top.php";
           </table>
      
     </div>
-  </div>
+ 
     
 </div>
 </div>
 
-
-
-                </div>
-                <div class="tab-pane fade" id="bordered-justified-profile" role="tabpanel" aria-labelledby="profile-tab">
-                  Nesciunt totam et. Consequuntur magnam aliquid eos nulla dolor iure eos quia. Accusantium distinctio omnis et atque fugiat. Itaque doloremque aliquid sint quasi quia distinctio similique. Voluptate nihil recusandae mollitia dolores. Ut laboriosam voluptatum dicta.
-                </div>
-                <div class="tab-pane fade" id="bordered-justified-contact" role="tabpanel" aria-labelledby="contact-tab">
-                  Saepe animi et soluta ad odit soluta sunt. Nihil quos omnis animi debitis cumque. Accusantium quibusdam perspiciatis qui qui omnis magnam. Officiis accusamus impedit molestias nostrum veniam. Qui amet ipsum iure. Dignissimos fuga tempore dolor.
-                </div>
-              </div><!-- End Bordered Tabs Justified -->
 
             </div>
           </div>
